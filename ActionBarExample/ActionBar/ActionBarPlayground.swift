ed0532048d8f12f465328f576c123621e802e036
Naziyok, Tolga <Tolga.Naziyok@cewe.de>
2022-04-18 12:14:40 +0200

    refactoring: ActionBarPlayground

--- ActionBarExample/ActionBar/ActionBarPlayground.swift
+++ ActionBarExample/ActionBar/ActionBarPlayground.swift
@@ -10,113 +10,88 @@ import Foundation
 import UIKit
 import PureLayout
 
-@objc final class ActionBarPlayground: UIViewController, UITableViewDelegate, UITableViewDataSource
+final class ActionBarPlayground: UIViewController, UITableViewDelegate, UITableViewDataSource
 {
-    var propertiesList: [ActionBarProperties] = []
+    // MARK: Private Properties
+    
+    private var propertiesList: [ActionBarProperties] = []
     private let tableView: UITableView = UITableView()
-    private var bottomSafeAreaContainer = UIView() // TODO
-    private var regularActionbar: ActionBarManager?
-    private var specialActionbar: ActionBarManager?
 
+}
+
+// MARK: UIKit
+
+extension ActionBarPlayground
+{
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        setupViews()
-        setupProperties()
+        setupTableView()
+        combineActionBarVariants()
+        
+        // Choose a Actionbar to display at the bottom
 //        setupRegularActionbar()
-        setupAlternativeProperties2()
-        setConstrains()
+//        setupSpecialActionbar1()
+        setupSpecialActionbar2()
     }
 
-    func setupViews()
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        propertiesList.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "test", for: indexPath)
+
+        cell.contentView.subviews.forEach { $0.removeFromSuperview() }
+        cell.selectionStyle = .none
+
+        let actionbarManager: ActionBarManager = ActionBarManager(properties: propertiesList[indexPath.row])
+
+        cell.contentView.addSubview(actionbarManager.actionBarView)
+
+        actionbarManager.actionBarView.autoPinEdgesToSuperviewEdges()
+
+        return cell
+    }
+
+}
+
+// MARK: - Private API
+
+private extension ActionBarPlayground
+{
+    func setupTableView()
     {
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "test")
         tableView.dataSource = self
         tableView.delegate = self
         tableView.backgroundColor = .darkGray
         tableView.separatorColor = .darkGray
+        
         view.addSubview(tableView)
-    }
-
-    func setConstrains()
-    {
+        
         tableView.autoPinEdgesToSuperviewEdges()
     }
-
-    // swiftlint:disable function_body_length
-    func setupProperties()
+    
+    /// First, create arrays of all setting variants. Second, combine all possible setting variants and store them in `propertiesList`.
+    func combineActionBarVariants()
     {
-        let ctaButton: ABP.CallToActionButton = ABP.CallToActionButton(
-            icon: UIImage.actions,
-            subtitle: "Test subtitle",
-            action: {
-                let viewController = UIViewController()
-                self.navigationController?.pushViewController(viewController, animated: true)
-            }
-        )
+        typealias ABP = ActionBarProperties
 
-        let editButton: ABP.TitleOrIconButton = ABP.TitleOrIconButton(
-            icon: UIImage.strokedCheckmark,
-            action: { print("edit Button pressed!") }
-        )
-
-        let backButton: ABP.TitleOrIconButton = ABP.TitleOrIconButton(
-            icon: UIImage.checkmark,
-            subtitle: "Test subtitle",
-            action: { print("back Button pressed!") }
-        )
-
-        let deleteButton: ABP.TitleOrIconButton = ABP.TitleOrIconButton(
-            icon: UIImage.remove,
-            tint: .systemRed,
-            action: { print("delete Button pressed!") }
-        )
-
-        let shareButton: ABP.TitleOrIconButton = ABP.TitleOrIconButton(
-            icon: UIImage.add,
-            subtitle: "Share",
-            tint: .systemGreen,
-            action: { print("abort Button pressed!") }
-        )
-
-        let editButtonText: ABP.TitleOrIconButton = ABP.TitleOrIconButton(
-            title: "Edit",
-            tint: ActionBarProperties.defaultTintColor,
-            action: { print("edit Button pressed!") }
-
-        )
-
-        let backButtonText: ABP.TitleOrIconButton = ABP.TitleOrIconButton(
-            title: "Back",
-            subtitle: "Test subtitle",
-            tint: ActionBarProperties.defaultTintColor,
-            action: { print("back Button pressed!") }
-        )
-
-        let deleteButtonText: ABP.TitleOrIconButton = ABP.TitleOrIconButton(
-            title: "Delete",
-            tint: .systemRed,
-            action: { print("delete Button pressed!") }
-        )
-
-        let shareButtonText: ABP.TitleOrIconButton = ABP.TitleOrIconButton(
-            title: "Share",
-            subtitle: "Share",
-            tint: .systemGreen,
-            action: { print("abort Button pressed!") }
-        )
-
-        let ctaSettings: [ABP.CallToActionButton?] = [ctaButton, nil]
+        let ctaSettings: [ABP.CallToActionButton?] = [ABP.ctaButton, nil]
         let ctaWidthSettings: [ABP.WidthPercentFormat] = [.twenty, .twentyfive, .thirty, .fifty]
         let buttonSettings: [[ABP.TitleOrIconButton]] = [
-            [editButton],
-            [editButton, backButton],
-            [editButton, backButton, deleteButton],
-            [editButton, backButton, deleteButton, shareButton],
-            [editButtonText],
-            [editButtonText, backButtonText],
-            [editButtonText, backButtonText, deleteButtonText],
-            [editButtonText, backButtonText, deleteButtonText, shareButtonText],
+            [ABP.editButton],
+            [ABP.editButton, ABP.backButton],
+            [ABP.editButton, ABP.backButton, ABP.deleteButton],
+            [ABP.editButton, ABP.backButton, ABP.deleteButton, ABP.shareButton],
+            [ABP.editButtonText],
+            [ABP.editButtonText, ABP.backButtonText],
+            [ABP.editButtonText, ABP.backButtonText, ABP.deleteButtonText],
+            [ABP.editButtonText, ABP.backButtonText, ABP.deleteButtonText, ABP.shareButtonText],
         ]
 
         for ctaSetting in ctaSettings
@@ -170,15 +145,12 @@ import PureLayout
 
         view.addSubview(actionbarManager.actionBarView)
 
-//        actionbarManager.bottomSafeAreaContainer.autoCreateConstraints()
         actionbarManager.actionBarView.autoPinEdge(toSuperviewEdge: .left)
         actionbarManager.actionBarView.autoPinEdge(toSuperviewEdge: .right)
         actionbarManager.actionBarView.autoPinEdge(toSuperviewSafeArea: .bottom)
-
-        regularActionbar = actionbarManager
     }
 
-    func setupAlternativeProperties1()
+    func setupSpecialActionbar1()
     {
         let alternativeABManager: ActionBarManager = ActionBarManager()
 
@@ -187,87 +159,48 @@ import PureLayout
         alternativeABManager.actionBarView.autoPinEdge(toSuperviewEdge: .left)
         alternativeABManager.actionBarView.autoPinEdge(toSuperviewEdge: .right)
         alternativeABManager.actionBarView.autoPinEdge(toSuperviewSafeArea: .bottom)
-
-        bottomSafeAreaContainer = alternativeABManager.actionBarView
 
         alternativeABManager.add(buttonType: .cta, title: "Power und ein sehr langer Titel", subtitle: "is <u>maxed</u>")
         {
             alternativeABManager.update(buttonType: .cta, subtitle: "")
         }
 
-//        alternativeABManager.update(buttonType: .cta, title: "Weakness")
-        alternativeABManager.update(buttonType: .cta, subtitle: "is <u>low</u>")
+        alternativeABManager.update(buttonType: .cta, title: "Weakness")
     }
 
-    // swiftlint:disable function_body_length
-    /// - Tag: PopoverExample1
-    func setupAlternativeProperties2()
+    func setupSpecialActionbar2()
     {
         let alternativeABManager: ActionBarManager = ActionBarManager()
 
         view.addSubview(alternativeABManager.actionBarView)
 
-//        alternativeABManager.bottomSafeAreaContainer.autoCreateConstraints()
         alternativeABManager.actionBarView.autoPinEdge(toSuperviewEdge: .left)
         alternativeABManager.actionBarView.autoPinEdge(toSuperviewEdge: .right)
         alternativeABManager.actionBarView.autoPinEdge(toSuperviewSafeArea: .bottom)
-
-        // example to hide actionbar on init
-//        alternativeABManager.hide()
-
-        bottomSafeAreaContainer = alternativeABManager.actionBarView
-
-        specialActionbar = alternativeABManager
 
         alternativeABManager.add(buttonType: .cta, title: "Power", subtitle: "maxed")
         {
             alternativeABManager.update(buttonType: .cta, subtitle: "")
         }
 
-        let managerAction: () -> Void = {
-            let label: UILabel = UILabel.newAutoLayout()
-            label.text = "Dies ist nur ein Test"
-            label.textColor = .black
-            label.textAlignment = .center
-            let icon: UIImageView = UIImageView()
-            icon.image = UIImage.actions
-            icon.contentMode = .center
-
-            let stackView: UIStackView = UIStackView(arrangedSubviews: [label, icon])
-            stackView.configureForAutoLayout()
-            stackView.axis = .vertical
-            stackView.distribution = .fillEqually
-            stackView.backgroundColor = .yellow
-
-            let alertController: UIAlertController = UIAlertController(title: "Attention", message: "Just a test", preferredStyle: .alert)
-            
-            self.present(alertController, animated: true)
-            {
-                let gesture = UITapGestureRecognizer(target: self, action: #selector(self.dismissAlertController))
-                alertController.view.superview?.addGestureRecognizer(gesture)
-            }
-        }
-
         guard let managerButtonIndex: ButtonIndex = alternativeABManager.add(
             buttonType: .featureButton,
             title: "Manager Button",
             subtitle: "löst Probleme",
-            action: managerAction
+            action: managerButtonAction
         ) else { return }
 
         alternativeABManager.update(buttonType: .featureButton, icon: UIImage.add, buttonIndex: managerButtonIndex)
-
-        let laserAction: () -> Void = {
-            alternativeABManager.hide(buttonType: .cta)
-            alternativeABManager.update(buttonType: .featureButton, tint: .green, buttonIndex: managerButtonIndex)
-        }
 
         guard let laserButtonIndex: ButtonIndex = alternativeABManager.add(
             buttonType: .featureButton,
             title: "Laser Beam",
             subtitle: "blendendes Argument",
             tint: .brown,
-            action: laserAction
+            action: {
+                alternativeABManager.hide(buttonType: .cta)
+                alternativeABManager.update(buttonType: .featureButton, tint: .green, buttonIndex: managerButtonIndex)
+            }
         ) else { return }
 
         alternativeABManager.update(buttonType: .cta, title: "Weakness")
@@ -275,34 +208,20 @@ import PureLayout
         alternativeABManager.update(buttonType: .featureButton, title: "Useless", subtitle: "schon immer", buttonIndex: managerButtonIndex)
         alternativeABManager.update(buttonType: .featureButton, subtitle: "hot", buttonIndex: laserButtonIndex)
     }
+    
+    func managerButtonAction()
+    {
+        let alertController: UIAlertController = UIAlertController(title: "Attention", message: "Just a test", preferredStyle: .alert)
+        
+        self.present(alertController, animated: true)
+        {
+            let gesture = UITapGestureRecognizer(target: self, action: #selector(self.dismissAlertController))
+            alertController.view.superview?.addGestureRecognizer(gesture)
+        }
+    }
 
     @objc func dismissAlertController(){
         self.dismiss(animated: true, completion: nil)
     }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
-    {
-        propertiesList.count
-    }
 
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
-    {
-        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "test", for: indexPath)
-
-        cell.contentView.subviews.forEach { $0.removeFromSuperview() }
-        cell.selectionStyle = .none
-
-        let actionbarManager: ActionBarManager = ActionBarManager(properties: propertiesList[indexPath.row])
-
-        cell.contentView.addSubview(actionbarManager.actionBarView)
-
-        actionbarManager.actionBarView.autoPinEdgesToSuperviewEdges()
-
-        return cell
-    }
-
-    @objc func dissmissController()
-    {
-        dismiss(animated: true, completion: nil)
-    }
 }
---
+++ ActionBarExample/ActionBar/ActionButtonVariants.swift
@@ -0,0 +1,70 @@
+//
+//  ActionButtonVariants.swift
+//  ActionBarExample
+//
+//  Created by Naziyok, Tolga on 18.04.22.
+//
+
+import Foundation
+import UIKit
+
+extension ActionBarProperties
+{
+    static let ctaButton: ABP.CallToActionButton = ABP.CallToActionButton(
+        icon: UIImage.actions,
+        subtitle: "Test subtitle",
+        action: { print("cta Button pressed!") }
+    )
+
+    static let editButton: ABP.TitleOrIconButton = ABP.TitleOrIconButton(
+        icon: UIImage.strokedCheckmark,
+        action: { print("edit Button pressed!") }
+    )
+
+    static let backButton: ABP.TitleOrIconButton = ABP.TitleOrIconButton(
+        icon: UIImage.checkmark,
+        subtitle: "Test subtitle",
+        action: { print("back Button pressed!") }
+    )
+
+    static let deleteButton: ABP.TitleOrIconButton = ABP.TitleOrIconButton(
+        icon: UIImage.remove,
+        tint: .systemRed,
+        action: { print("delete Button pressed!") }
+    )
+
+    static let shareButton: ABP.TitleOrIconButton = ABP.TitleOrIconButton(
+        icon: UIImage.add,
+        subtitle: "Share",
+        tint: .systemGreen,
+        action: { print("abort Button pressed!") }
+    )
+
+    static let editButtonText: ABP.TitleOrIconButton = ABP.TitleOrIconButton(
+        title: "Edit",
+        tint: ActionBarProperties.defaultTintColor,
+        action: { print("edit Button pressed!") }
+
+    )
+
+    static let backButtonText: ABP.TitleOrIconButton = ABP.TitleOrIconButton(
+        title: "Back",
+        subtitle: "Test subtitle",
+        tint: ActionBarProperties.defaultTintColor,
+        action: { print("back Button pressed!") }
+    )
+
+    static let deleteButtonText: ABP.TitleOrIconButton = ABP.TitleOrIconButton(
+        title: "Delete",
+        tint: .systemRed,
+        action: { print("delete Button pressed!") }
+    )
+
+    static let shareButtonText: ABP.TitleOrIconButton = ABP.TitleOrIconButton(
+        title: "Share",
+        subtitle: "Share",
+        tint: .systemGreen,
+        action: { print("abort Button pressed!") }
+    )
+
+}
